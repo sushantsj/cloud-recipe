214d0cf97b4f801b8d1ba166b54772977a331fb0
sushantsj <sushantjhasep@gmail.com>
2022-04-11 10:29:16 +0100

    added another external rest api

--- cloud-recipe/recipe/src/main/java/com/cloud/recipe/controller/YoutubeController.java
+++ cloud-recipe/recipe/src/main/java/com/cloud/recipe/controller/YoutubeController.java
@@ -1,23 +1,26 @@
 package com.cloud.recipe.controller;
 
-
+import java.util.Arrays;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.cloud.recipe.entity.YoutubeVideo;
 import com.cloud.recipe.serviceimpl.YoutubeService;
+import com.fasterxml.jackson.databind.JsonNode;
 
 @RestController
 @RequestMapping("/api/externalapis")
 public class YoutubeController {
-	
+
 	private final Logger log = LoggerFactory.getLogger(YoutubeController.class);
 
 	@Autowired
@@ -39,9 +42,20 @@ public class YoutubeController {
 	@GetMapping("/youtubeDemo/{query}")
 	public List<YoutubeVideo> formSubmit(@PathVariable("query") String query) {
 		log.info("Get videos link by query: {}", query);
+		log.info("Create your own youtube video api key and update in youtube service file");
 		// get the list of YouTube videos that match the search term
 		List<YoutubeVideo> videos = youtubeService.fetchVideosByQuery(query);
 		log.info("videos list: {}", videos.toString());
 		return videos;
+	}
+
+	@GetMapping("/public-api")
+	public List<Object> getCountries() {
+		log.info("Enter your api key generated on the moviedb website and update below");
+		String url = "https://api.themoviedb.org/3/movie/550?api_key = <update your api key>";
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<JsonNode> countries = restTemplate.getForEntity(url, JsonNode.class);
+		return Arrays.asList(countries);
+
 	}
 }
